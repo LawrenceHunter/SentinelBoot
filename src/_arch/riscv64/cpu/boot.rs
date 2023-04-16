005dad53e90444c8bc832ac98ec009d15be3ebd3
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-04-16 21:41:33 +0100

    Attempting to get output
    Various modifications to bootloader.ld and boot.rs assembly

--- src/_arch/riscv64/cpu/boot.rs
+++ src/_arch/riscv64/cpu/boot.rs
@@ -7,19 +7,37 @@
 //!
 //! crate::cpu::boot::arch_boot
 
-use core::arch::global_asm;
+const BOOT_STACK_SIZE: usize = 0x10000 * 5;
 
-// Assembly counterpart to this file.
-global_asm!(
-    include_str!("boot.s"),
-    // CONST_CORE_ID_MASK = const 0b11
-);
+static mut BOOT_STACK: [u8; BOOT_STACK_SIZE] = [0; BOOT_STACK_SIZE];
+
+#[naked]
+#[link_section = ".text.entry"]
+#[export_name = "_start"]
+unsafe extern "C" fn entry() -> ! {
+    core::arch::asm!("
+    # 1. set sp
+    # sp = bootstack + (hartid + 1) * 0x10000
+    add     t0, a0, 1
+    slli    t0, t0, 14
+1:  auipc   sp, %pcrel_hi({boot_stack})
+    addi    sp, sp, %pcrel_lo(1b)
+    add     sp, sp, t0
+    # 2. jump to rust_main (absolute address)
+1:  auipc   t0, %pcrel_hi({rust_main})
+    addi    t0, t0, %pcrel_lo(1b)
+    jr      t0
+    ",
+    boot_stack = sym BOOT_STACK,
+    rust_main = sym _start_rust,
+    options(noreturn))
+}
 
 //--------------------------------------------------------------------------------------------------
 // Public Code
 //--------------------------------------------------------------------------------------------------
 
 #[no_mangle]
-pub unsafe fn _start_rust() -> ! {
-    crate::kernel_init()
+pub unsafe fn _start_rust(hartid: usize, dtb_pa: usize) -> ! {
+    crate::loader_init(hartid: usize, dtb_pa: usize)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-//! The `kernel` binary.
+//! The `bootloader` binary.
 //!
-//! 1. The kernel's entry point is the function `cpu::boot::arch_boot::_start()`.
+//! 1. The bootloader's entry point is the function `cpu::boot::arch_boot::_start()`.
 //!     - It is implemented in `src/_arch/__arch_name__/cpu/boot.s`.
 //! 2. Once finished with architectural setup, the arch code calls `kernel_init()`.
 
 #![allow(clippy::upper_case_acronyms)]
-#![feature(asm_const)]
+#![feature(naked_functions, asm_const, type_ascription)]
 #![feature(format_args_nl)]
 #![feature(panic_info_message)]
 #![feature(trait_alias)]
@@ -24,8 +24,8 @@ mod synchronisation;
 ///
 /// # Safety
 ///
-/// - Only a single core must be active and running this function.
-unsafe fn kernel_init() -> ! {
+/// - Only a single hart must be active and running this function.
+unsafe fn loader_init(_hartid: usize, _dtb_pa: usize) -> ! {
     // Initialise BSP driver subsystem
     if let Err(x) = bsp::driver::init() {
         panic!("Error intialising BSP driver subsystem: {}", x);
@@ -36,11 +36,11 @@ unsafe fn kernel_init() -> ! {
     // println! usable from here
 
     // Transition from unsafe to safe
-    kernel_main()
+    loader_main()
 }
 
 // Main function running after early init
-fn kernel_main() -> ! {
+fn loader_main() -> ! {
     use console::console;
     println!(
         "[0] {} version {}",
