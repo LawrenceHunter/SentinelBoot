30183db2ac2ed4fe797b0165c54cb59471c89bbf
Lawrence Hunter <lawrence.hunter@codethink.co.uk>
2023-07-11 09:50:26 +0100

    wip! state of 10/07/23 try to get a jtag board

--- tftp/mkimage.sh
+++ tftp/mkimage.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-mkimage -A riscv -T invalid -O invalid -C none -a 0 -e 0 -d sdcard.img uImage
+mkimage -A riscv -T script -O linux -C none -d boot.scr boot.scr.uimg
+mkimage -A riscv -T kernel -O linux -C none -d bootloader.img -a 0x80000000 -e 0x80000000 Image
