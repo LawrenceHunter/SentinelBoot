b32dd9fd0abcf5f92207c0f4af69a08407695326
Lawrence Hunter <lawrence.hunter@codethink.co.uk>
2023-07-18 16:44:56 +0100

    state 180723

--- tftp/setup_bridge.sh
+++ tftp/setup_bridge.sh
@@ -6,6 +6,9 @@ brctl addif br0 eth1
 tunctl -t tap0 -u `whoami`
 brctl addif br0 tap0
 ifconfig eth1 up
+ifconfig eth1 192.168.0.1
 ifconfig tap0 up
 ifconfig br0 up
 ifconfig br0 192.168.0.1
+systemctl restart isc-dhcp-server
+/etc/init.d/tftpd-hpa restart
