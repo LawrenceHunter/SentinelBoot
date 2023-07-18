eb5a0542235f661d0dc66485fa9e9379a43024d1
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-07-18 18:24:50 +0100

    Add visionfive 2 tftp boot test

---
+++ tftp/mkimage.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+mkimage -A riscv -T script -O efi -C none -d boot.scr boot.scr.uimg
---
+++ tftp/vf2_tftp.py
@@ -0,0 +1,26 @@
+import serial
+
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.5)
+
+tests = [("❕ Waiting for 'Hit any key to stop autoboot'...",
+          "✅ Got Hit any key to stop autoboot'",
+          "".encode(),
+          "Hit any key to stop autoboot")]
+
+test = 0
+
+if __name__ == "__main__":
+    print(tests[test][0])
+    while test < len(tests):
+        while ser.in_waiting:
+            data = ser.readline().decode("ascii")
+            if tests[test][3] in data:
+                state = 1
+                print(tests[test][1])
+                test += 1
+                try:
+                    print(tests[test][0])
+                except IndexError:
+                    break
+                ser.write(tests[test][2].encode())
+    print("✅ All expected output achieved!")
