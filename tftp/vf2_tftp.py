3239d6847b13c4a9d2c672892b1e74f21dedf537
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-08-17 22:10:22 +0100

    Match tftp testing to qemu_test

--- tftp/vf2_tftp.py
+++ tftp/vf2_tftp.py
@@ -3,9 +3,13 @@ import serial
 ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=0.5)
 
 tests = [("❕ Waiting for 'Hit any key to stop autoboot'...",
-          "✅ Got Hit any key to stop autoboot'",
+          "✅ Got 'Hit any key to stop autoboot'",
           "".encode(),
-          "Hit any key to stop autoboot")]
+          "Hit any key to stop autoboot"),
+          ("❕ Waiting for 'Echoing input now'...",
+          "✅ Got 'Echoing input now'",
+          "".encode(),
+          "Echoing input now")]
 
 test = 0
 
@@ -14,6 +18,7 @@ if __name__ == "__main__":
     while test < len(tests):
         while ser.in_waiting:
             data = ser.readline().decode("ascii")
+            print(f"\t {data}")
             if tests[test][3] in data:
                 state = 1
                 print(tests[test][1])
@@ -22,5 +27,5 @@ if __name__ == "__main__":
                     print(tests[test][0])
                 except IndexError:
                     break
-                ser.write(tests[test][2].encode())
+                ser.write(tests[test][2])
     print("✅ All expected output achieved!")
