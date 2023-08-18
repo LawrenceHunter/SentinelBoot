4280aa05976e1736d062a4276ae8211a98a32180
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-08-18 20:24:29 +0100

    Fix: CI failures

--- src/helper.rs
+++ src/helper.rs
@@ -13,4 +13,4 @@ pub fn print_boot_logo() {
 	println!(r"              \/_/                                                           ");
 	println!(r"");
 }
-pub const SHA: &str = "20c74052";
+pub const SHA: &str = "c0d90bb9";
