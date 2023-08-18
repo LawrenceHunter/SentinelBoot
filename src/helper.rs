c0d90bb9793388518f10182399ea92ca9679d86e
Lawrence Hunter <lawrence.hunter@codethink.co.uk>
2023-08-18 15:10:55 +0100

    Remove boot.scr with burden on actions

--- src/helper.rs
+++ src/helper.rs
@@ -13,4 +13,4 @@ pub fn print_boot_logo() {
 	println!(r"              \/_/                                                           ");
 	println!(r"");
 }
-pub const SHA: &str = "786485de";
+pub const SHA: &str = "20c74052";
