c5ce7db1fec02ae6401a8f2d7d2a6484839dde87
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-12-03 18:07:07 +0000

    Working signature verification

--- src/helper.rs
+++ src/helper.rs
@@ -13,5 +13,5 @@ pub fn print_boot_logo() {
 	println!(r"              \/_/                                                           ");
 	println!(r"");
 }
-pub const SHA: &str = "7895a3ab";
-pub const  PUBLIC_KEY: &'static [u8] = include_bytes!("../tftp/public_key.raw");
+pub const SHA: &str = "461f9d9a";
+pub const  PUBLIC_KEY: &'static [u8] = include_bytes!("../tftp/public_key.pem");
--- tftp/sign_kernel.sh
+++ tftp/sign_kernel.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-python3 generate_rsa_keys.py && \
+python3 generate_ed25519_keys.py && \
 gzip --decompress --keep Image.gz && \
 python3 hash.py Image && \
 gzip Image_signed && rm Image
