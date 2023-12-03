a1184d633ee0df775e6d42ac0165309318a6e02a
Lawrence Hunter <lawrence.hunter@outlook.com>
2023-12-03 13:34:24 +0000

    Runtime cryptography - invalid signature

--- src/main.rs
+++ src/main.rs
@@ -109,15 +109,6 @@ fn loader_main() {
 
     unsafe {
         let mut data: u128;
-        let mut address: usize = bsp::memory::map::kernel::PUBK;
-        for _ in 0..10 {
-            data = core::ptr::read(address as *mut u128);
-            println!("{:#010x}: {:>#034x}", address, data);
-            address += 0x10
-        }
-    }
-    unsafe {
-        let mut data: u128;
         let mut address: usize = bsp::memory::map::kernel::KERNEL - 0x100;
         for _ in 0..10 {
             data = core::ptr::read(address as *mut u128);
@@ -158,7 +149,7 @@ fn loader_main() {
     println!("Instantiated hasher.");
 
     let mut offset = 0;
-    let buff_size = 128;
+    let buff_size = 4096;
     loop {
         let data = unsafe {
             slice::from_raw_parts(
@@ -167,7 +158,8 @@ fn loader_main() {
                 buff_size,
             )
         };
-        if data.iter().all(|&x| x == 0) {
+        // This is the problem how at runtime do I detect 13365248 bytes
+        if (offset * buff_size) >= 13365248 {
             break;
         }
         hasher.update(data);
@@ -176,16 +168,27 @@ fn loader_main() {
 
     println!("Hasher update finished.");
     let hash = hasher.finalize();
-    println!("Binary hash: {:X?}", hash);
+    println!("Binary hash: \n{:X?}", hash);
     let expected: [u8; 32] = [
         0x6A, 0xD, 0x64, 0x99, 0x6D, 0x62, 0x74, 0xBA, 0x39, 0x8B, 0x5B, 0x64,
         0x18, 0x88, 0x5E, 0x9B, 0x2E, 0xF0, 0xA, 0x16, 0x45, 0xD7, 0xFB, 0x37,
         0x6C, 0x75, 0x76, 0xCF, 0xA, 0xC4, 0xA9, 0x53,
     ];
-    println!("Expected hash: {:X?}", expected);
+    println!("Expected hash: \n{:X?}", expected);
     assert_eq_hex!(expected, hash.as_slice());
+    println!();
 
-    loop {}
+    let public_key = ed25519_compact::PublicKey::from_slice(crate::helper::PUBLIC_KEY).unwrap();
+    println!("{:?}\n", public_key);
+    let signature_bytes = unsafe { slice::from_raw_parts(
+        (bsp::memory::map::kernel::KERNEL - 0x100)
+            as *mut u8,
+        64,
+    ) };
+    let signature = ed25519_compact::Signature::from_slice(signature_bytes).unwrap();
+    println!("Signature {:?}\n", signature);
+    let verified = public_key.verify(hash.as_slice(), &signature);
+    println!("Verified: {:?}", verified);
 
     // println!("XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX");
     // println!("X                          ENTERING MACHINE MODE
