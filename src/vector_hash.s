f6f8ddee6e59afc9a6ef5f7de6ff186085cd5b7f
Lawrence Hunter <lawrence.hunter@outlook.com>
2024-02-06 22:37:37 +0000

    WIP: Add placeholder for full SHA

--- src/vector_hash.s
+++ src/vector_hash.s
@@ -1,48 +1,152 @@
-# SEW = e64 (64 bytes)
-# LMUL = 1
-# a0 contains length of vector
-# a1 contains pointer to kernel
-# a2 contains size of kernel
-# I think as SHA-256 I'll need a0 to be 4 not 8
-li a0, 4
-add a1, s1, zero
-# add a2, {kernel_size}, zero
-li a2, 96
-# Sanity check forms a loop counter
-li a3, 0
-mv a4, v0
-mv a5, v1
-mv a6, v2
-mv a7, v3
-hash_kernel_asm:
-    # Set vector length to 4 64 bit elements
-    vsetvli t0, a0, e64, m1, tu, mu
-    # Load 4 consecutive 64 bit values into v0-v7
-    vle64.v v0, (a1)
-    # Increment a1 to point to the next 64 byte chunk
-    addi a1, a1, 64
-    # Decrement a2 to reduce remaining size by 64 bytes
-    addi a2, a2, -64
-    hash_kernel_asm_internal_loop:
-        add a3, a3, 1
-        # Load 4 consecutive 64 bit values into v8-v15
-        vle64.v v4, (a1)
-        # Increment a1 to point to the next 32 byte chunk
-        addi a1, a1, 32
-        # Decrement a2 to reduce remaining size by 32 bytes
-        addi a2, a2, -32
-        # Load 4 consecutive 64 bit values into v16-v23
-        vle64.v v8, (a1)
-        # Increment a1 to point to the next 32 byte chunk
-        addi a1, a1, 32
-        # Decrement a2 to reduce remaining size by 32 bytes
-        addi a2, a2, -32
-        # vsha2ms.vv v0, v8, v4
-        # 101101 1 10000 01000 010 00000 1110111
-        .word 0xB7042077
-    blt a2, zero, hash_kernel_asm_internal_loop
-add s2, a3, zero
-mv v0, a4
-mv v1, a5
-mv v2, a6
-mv v3, a7
+#  Set vector configuration
+vsetivli x0, 4, e32, m1, ta, ma
+
+# Load 512 bits of the message block into v10-v13 endian swaping
+vle32.v v10, (a1)
+vrev8.v v10, v10
+vle32.v v11, (a1)
+vrev8.v v11, v11
+vle32.v v12, (a1)
+vrev8.v v12, v12
+vle32.v v13, (a1)
+vrev8.v v13, v13
+
+# Load H[0..8]
+vle32.v v16, (a0)
+vle32.v v17, (a0)
+
+# Capture initial H to allow computing H'
+vmv.v.v v26, v16
+vmv.v.v v27, v17
+
+# Set v0 for vmerge that replaces first word
+vid.v v0
+vmseq.vi v0, v0, 0x0
+
+# Round 0
+vle32.v v15, (a2)
+vadd.vv v14, v15, v10
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v12, v11, v0
+# vsha2ms.vv v10, v14, v13
+
+# Round 1
+vle32.v v15, (a2)
+vadd.vv v14, v15, v11
+vsha2cl.vv v17, v16, v14
+vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v13, v12, v0
+vsha2ms.vv v11, v14, v10
+
+# Round 2
+vle32.v v15, (a2)
+vadd.vv v14, v15, v12
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v10, v13, v0
+# vsha2ms.vv v12, v14, v11
+
+# Round 3
+vle32.v v15, (a2)
+vadd.vv v14, v15, v13
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v11, v10, v0
+# vsha2ms.vv v13, v14, v12
+
+# Round 4
+vle32.v v15, (a2)
+vadd.vv v14, v15, v10
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v12, v11, v0
+# vsha2ms.vv v10, v14, v13
+
+# Round 5
+vle32.v v15, (a2)
+vadd.vv v14, v15, v11
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v13, v12, v0
+# vsha2ms.vv v11, v14, v10
+
+# Round 6
+vle32.v v15, (a2)
+vadd.vv v14, v15, v12
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v10, v13, v0
+# vsha2ms.vv v12, v14, v11
+
+# Round 7
+vle32.v v15, (a2)
+vadd.vv v14, v15, v13
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v11, v10, v0
+# vsha2ms.vv v13, v14, v12
+
+# Round 8
+vle32.v v15, (a2)
+vadd.vv v14, v15, v10
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v12, v11, v0
+# vsha2ms.vv v10, v14, v13
+
+# Round 9
+vle32.v v15, (a2)
+vadd.vv v14, v15, v11
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v13, v12, v0
+# vsha2ms.vv v11, v14, v10
+
+# Round 10
+vle32.v v15, (a2)
+vadd.vv v14, v15, v12
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v10, v13, v0
+# vsha2ms.vv v12, v14, v11
+
+# Round 11
+vle32.v v15, (a2)
+vadd.vv v14, v15, v13
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+vmerge.vvm v14, v11, v10, v0
+# vsha2ms.vv v13, v14, v12
+
+# Round 12
+vle32.v v15, (a2)
+vadd.vv v14, v15, v10
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+
+# Round 13
+vle32.v v15, (a2)
+vadd.vv v14, v15, v11
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+
+# Round 14
+vle32.v v15, (a2)
+vadd.vv v14, v15, v12
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+
+# Round 15
+vle32.v v15, (a2)
+vadd.vv v14, v15, v13
+# vsha2cl.vv v17, v16, v14
+# vsha2ch.vv v16, v17, v14
+
+# Update the hash
+add.vv v16, v26, v16
+vadd.vv v17, v27, v17
+
+# Save the hash
+vse32.v v17, (a0)
+vse32.v v16, (a0)
--- src/verification.rs
+++ src/verification.rs
@@ -4,6 +4,362 @@ use pelite::pe64::{self, Pe};
 // #[cfg(not(feature = "qemu"))]
 use sha2::{Digest, Sha256};
 
+/* -------------------------------------------------------------------------- */
+/*                            Serial Implementation                           */
+/* -------------------------------------------------------------------------- */
+
+#[cfg(not(feature = "qemu"))]
+fn hash_kernel() -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    let mut offset = 0;
+    let buff_size = 4096;
+    let kernel_size = get_kernel_size();
+    println!(
+        "Kernel range: 0x{:X?} -> 0x{:X?}",
+        bsp::memory::map::kernel::KERNEL,
+        bsp::memory::map::kernel::KERNEL + kernel_size
+    );
+    loop {
+        let data = unsafe {
+            slice::from_raw_parts(
+                (bsp::memory::map::kernel::KERNEL + (offset * buff_size))
+                    as *mut u8,
+                buff_size,
+            )
+        };
+        if (offset * buff_size) >= kernel_size {
+            break;
+        }
+        hasher.update(data);
+        offset += 1;
+    }
+    hasher.finalize().into()
+}
+
+
+/* -------------------------------------------------------------------------- */
+/*                     Vector Cryptography Implementation                     */
+/* -------------------------------------------------------------------------- */
+
+// Temporary
+#[cfg(feature = "qemu")]
+fn hash_kernel_serial() -> [u8; 32] {
+    let mut hasher = Sha256::new();
+    let mut offset = 0;
+    let buff_size = 4096;
+    let kernel_size = get_kernel_size();
+    println!(
+        "Kernel range: 0x{:X?} -> 0x{:X?}",
+        bsp::memory::map::kernel::KERNEL,
+        bsp::memory::map::kernel::KERNEL + kernel_size
+    );
+    loop {
+        let data = unsafe {
+            slice::from_raw_parts(
+                (bsp::memory::map::kernel::KERNEL + (offset * buff_size))
+                    as *mut u8,
+                buff_size,
+            )
+        };
+        if (offset * buff_size) >= kernel_size {
+            break;
+        }
+        hasher.update(data);
+        offset += 1;
+    }
+    hasher.finalize().into()
+}
+
+#[cfg(feature = "qemu")]
+const SHA256_ROUND_CONSTANTS: [u32; 64] = [
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, // 0-3
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, // 4-7
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, // 8-11
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, // 12-15
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, // 16-19
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da, // 20-23
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, // 24-27
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, // 28-31
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, // 32-35
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, // 36-39
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, // 40-43
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, // 44-47
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, // 48-51
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, // 52-55
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, // 56-59
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2, // 60-63
+];
+
+#[cfg(feature = "qemu")]
+fn hash_kernel() -> [u8; 32] {
+    use core::arch::asm;
+
+    let mut kernel_size = get_kernel_size();
+    println!(
+        "Kernel range: 0x{:X?} -> 0x{:X?}",
+        bsp::memory::map::kernel::KERNEL,
+        bsp::memory::map::kernel::KERNEL + kernel_size
+    );
+    let mut result: [u32; 8] = [0, 0, 0, 0, 0, 0, 0, 0];
+    let mut loops = 0;
+
+    // ? Note this code cannot be used but is here for completeness
+    //      The U-boot which sits before QEMU to prevent the need to implement
+    //      a driver for tftp booting does not support vector operations and
+    //      as it hands operation to use in supervisor mode it is not possible
+    //      for us to enable this bit as such for development the require_rvv
+    //      check in QEMU is hardcode to return true.
+    // println!("Attempting enabling mstatus.vs");
+    // unsafe {
+    //     asm!(
+    //         "csrr t0, mstatus",
+    //         "ori t0, t0, 0x200",
+    //         "csrrw x0, mstatus, t0",
+    //         out("t0") _,
+    //     )
+    // }
+
+    println!("Attempting vector hashing - kernel size: {}B", kernel_size);
+    // ? Rust's RISC-V targets do not support vector operations nor vector
+    // ? cryptography operations as such the instructions are pre-assembled
+    // ? into their binary equivalents and hardcoded the comments represent
+    // ? the assembled instruction this solely bypasses the assembler and does
+    // ? not further negate security guarantees.
+
+    // Reference: RISC-V Cryptographic Extensions Vector Code Sample
+    // The code sample is part of the riscv-crypto project on GitHub
+    // commit 6589bcd6edb5abd91e758a67b28ae05b347c0470.
+    // See: https://github.com/riscv/riscv-crypto/blob/
+    //  6589bcd6edb5abd91e758a67b28ae05b347c0470/doc/vector/code-samples/zvknh.s
+
+    while kernel_size >= 32 {
+        unsafe {
+            asm!(
+                /* ------------------------- Sanity ------------------------- */
+                "addi a3, a3, 1",
+
+                /* ------------------------- Setup -------------------------- */
+                // Set vector configuration
+                // vsetivli x0, 4, e32, m1, ta, ma
+
+                // Load 512 bits of the message block into v10-v13 endian swaping
+                // vle32.v v10, (a1)
+                // vrev8.v v10, v10
+                add a1, a1, 16,
+                // vle32.v v11, (a1)
+                // vrev8.v v11, v11
+                add a1, a1, 16,
+                // vle32.v v12, (a1)
+                // vrev8.v v12, v12
+                add a1, a1, 16,
+                // vle32.v v13, (a1)
+                // vrev8.v v13, v13
+
+                // Load H[0..8]
+                // v26 = v16 = {a[t],b[t],e[t],f[t]}
+                // v27 = v17 = {c[t],d[t],g[t],h[t]}
+                // vle32.v v16, (a0)
+                addi a0, a0, 16,
+                // vle32.v v17, (a0)
+
+                // Capture initial H to allow computing H'
+                // vmv.v.v v26, v16
+                // vmv.v.v v27, v17
+
+                // Set v0 for vmerge that replaces first word
+                // v0.mask[i] = (i == 0 ? 1 : 0)
+                // vid.v v0
+                // vmseq.vi v0, v0, 0x0
+
+                /* ----------------------- Quad rounds ---------------------- */
+                // Round 0
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v10
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v12, v11, v0
+                // vsha2ms.vv v10, v14, v13
+
+                // Round 1
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v11
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v13, v12, v0
+                // vsha2ms.vv v11, v14, v10
+
+                // Round 2
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v12
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v10, v13, v0
+                // vsha2ms.vv v12, v14, v11
+
+                // Round 3
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v13
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v11, v10, v0
+                // vsha2ms.vv v13, v14, v12
+
+                // Round 4
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v10
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v12, v11, v0
+                // vsha2ms.vv v10, v14, v13
+
+                // Round 5
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v11
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v13, v12, v0
+                // vsha2ms.vv v11, v14, v10
+
+                // Round 6
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v12
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v10, v13, v0
+                // vsha2ms.vv v12, v14, v11
+
+                // Round 7
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v13
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v11, v10, v0
+                // vsha2ms.vv v13, v14, v12
+
+                // Round 8
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v10
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v12, v11, v0
+                // vsha2ms.vv v10, v14, v13
+
+                // Round 9
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v11
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v13, v12, v0
+                // vsha2ms.vv v11, v14, v10
+
+                // Round 10
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v12
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v10, v13, v0
+                // vsha2ms.vv v12, v14, v11
+
+                // Round 11
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v13
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+                // vmerge.vvm v14, v11, v10, v0
+                // vsha2ms.vv v13, v14, v12
+
+                // Round 12
+                // We no longer generate new message schedules
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v10
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+
+                // Round 13
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v11
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+
+                // Round 14
+                // vle32.v v15, (a2)
+                addi a2, a2, 16,
+                // vadd.vv v14, v15, v12
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+
+                // Round 15
+                // a2 increment not needed
+                // vle32.v v15, (a2)
+                // vadd.vv v14, v15, v13
+                // vsha2cl.vv v17, v16, v14
+                // vsha2ch.vv v16, v17, v14
+
+                /* --------------------- Update the hash -------------------- */
+                // vadd.vv v16, v26, v16
+                // vadd.vv v17, v27, v17
+
+                /* ---------------------- Save the hash --------------------- */
+                // vse32.v v17, (a0)
+                addi a0, a0, -16,
+                // vse32.v v16, (a0)
+                in("a0") bsp::memory::map::kernel::KERNEL + ((loops + 1) * 64),
+                in("a1") result.as_mut_ptr(),
+                in("a2") SHA256_ROUND_CONSTANTS.as_ptr(),
+                inout("a3") loops => loops,
+            );
+        }
+        kernel_size -= 64;
+    }
+
+    println!("Result: ({} iterations)", loops);
+
+    // Temporary for comparison to known
+    pretty_print_slice(
+        unsafe {
+            slice::from_raw_parts(result.as_ptr() as *mut u8, 32)
+        }
+    );
+    println!("Serial result: ");
+    pretty_print_slice(&hash_kernel_serial());
+    println!("Returned from vector hashing");
+    panic!("HALT");
+}
+
+/* -------------------------------------------------------------------------- */
+/*                                Unified code                                */
+/* -------------------------------------------------------------------------- */
+fn pretty_print_slice(bytes: &[u8]) {
+    let mut counter = 0;
+    let mut lines = 0;
+    let column_limit = 16;
+    print!("\r\n{:#04x} | ", lines * column_limit);
+    for byte in bytes {
+        if counter == column_limit {
+            counter = 0;
+            lines += 1;
+            print!("\r\n{:#04x} | ", lines * column_limit);
+        }
+        counter += 1;
+        print!("{:#04x} ", byte);
+    }
+    println!();
+}
+
 pub fn verify_kernel() -> Result<(), ed25519_compact::Error> {
     println!("Hashing stored kernel...");
 
@@ -47,138 +403,4 @@ fn get_kernel_size() -> usize {
     let kernel_size: usize = pe.optional_header().AddressOfEntryPoint as usize;
     println!("Kernel size: 0x{:X?}", kernel_size);
     64 * (kernel_size / 64) // normalises to multiple of 64
-}
-
-#[cfg(not(feature = "qemu"))]
-fn hash_kernel() -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    let mut offset = 0;
-    let buff_size = 4096;
-    let kernel_size = get_kernel_size();
-    println!(
-        "Kernel range: 0x{:X?} -> 0x{:X?}",
-        bsp::memory::map::kernel::KERNEL,
-        bsp::memory::map::kernel::KERNEL + kernel_size
-    );
-    loop {
-        let data = unsafe {
-            slice::from_raw_parts(
-                (bsp::memory::map::kernel::KERNEL + (offset * buff_size))
-                    as *mut u8,
-                buff_size,
-            )
-        };
-        if (offset * buff_size) >= kernel_size {
-            break;
-        }
-        hasher.update(data);
-        offset += 1;
-    }
-    hasher.finalize().into()
-}
-
-fn hash_kernel_serial() -> [u8; 32] {
-    let mut hasher = Sha256::new();
-    let mut offset = 0;
-    let buff_size = 4096;
-    let kernel_size = get_kernel_size();
-    println!(
-        "Kernel range: 0x{:X?} -> 0x{:X?}",
-        bsp::memory::map::kernel::KERNEL,
-        bsp::memory::map::kernel::KERNEL + kernel_size
-    );
-    loop {
-        let data = unsafe {
-            slice::from_raw_parts(
-                (bsp::memory::map::kernel::KERNEL + (offset * buff_size))
-                    as *mut u8,
-                buff_size,
-            )
-        };
-        if (offset * buff_size) >= kernel_size {
-            break;
-        }
-        hasher.update(data);
-        offset += 1;
-    }
-    hasher.finalize().into()
-}
-
-
-#[cfg(feature = "qemu")]
-fn hash_kernel() -> [u8; 32] {
-    use core::arch::asm;
-
-    let mut kernel_size = get_kernel_size();
-    println!(
-        "Kernel range: 0x{:X?} -> 0x{:X?}",
-        bsp::memory::map::kernel::KERNEL,
-        bsp::memory::map::kernel::KERNEL + kernel_size
-    );
-    let mut result: [u64; 4] = [0, 0, 0, 0];
-    result[0] = unsafe { core::ptr::read(bsp::memory::map::kernel::KERNEL as *const u64) };
-    result[1] = unsafe { core::ptr::read((bsp::memory::map::kernel::KERNEL + 8) as *const u64) };
-    result[2] = unsafe { core::ptr::read((bsp::memory::map::kernel::KERNEL + 16) as *const u64) };
-    result[3] = unsafe { core::ptr::read((bsp::memory::map::kernel::KERNEL + 24) as *const u64) };
-    let mut loop_count = 1;
-    // kernel_size = 256;
-    // println!("Attempting enabling mstatus.vs");
-    // unsafe {
-    //     asm!(
-    //         "csrr t0, mstatus",
-    //         "ori t0, t0, 0x200",
-    //         "csrrw x0, mstatus, t0",
-    //         out("t0") _,
-    //     )
-    // }
-    println!("Attempting vector hashing - kernel size: {}B", kernel_size);
-    println!("\tResult: {:?}", result);
-    while kernel_size >= 32 {
-        let kernel_pointer = bsp::memory::map::kernel::KERNEL + (loop_count * 64);
-        unsafe {
-            asm!(
-                "li a0, 8",
-                "addi a2, a2, 1",
-                // ".word 0x01a57a77", // vsetvli t0, a0, e32, m1, tu, mu
-                ".word 0x010572d7", // vsetvli t0, a0, e32, m1, tu, mu
-                ".word 0x0206f007", // vle64.v v0, (a3)
-                ".word 0x0205f207", // vle64.v v4, (a1)
-                "add a1, a1, 32",   // Sanity check
-                ".word 0x0205f407", // vle64.v v8, (a1)
-                ".word 0xB6822077", // vsha2ms.vv v0, v8, v4
-                ".word 0x0206f027", // vse64.v v0, (a3)
-                out("a0") _,
-                in("a1") kernel_pointer,
-                // Could be handled in rust but this is largely a sanity check
-                inout("a2") loop_count => loop_count,
-                in("a3") result.as_mut_ptr(),
-            );
-        }
-        kernel_size -= 64;
-        // println!("Loop performed: {}B, {}", kernel_size, loop_count - 1);
-    }
-
-    println!("Result: ");
-    pretty_print_slice(unsafe { slice::from_raw_parts(result.as_ptr() as *mut u8, 32) });
-    println!("Serial result: ");
-    pretty_print_slice(&hash_kernel_serial());
-    println!("Returned from vector hashing");
-    panic!("HALT");
-}
-
-fn pretty_print_slice(bytes: &[u8]) {
-    let mut counter = 0;
-    let mut lines = 0;
-    let column_limit = 16;
-    print!("\r\n{:#04x} | ", lines * column_limit);
-    for byte in bytes {
-        if counter == column_limit {
-            counter = 0;
-            lines += 1;
-            print!("\r\n{:#04x} | ", lines * column_limit);
-        }
-        counter += 1;
-        print!("{:#04x} ", byte);
-    }
-    println!();
 }
